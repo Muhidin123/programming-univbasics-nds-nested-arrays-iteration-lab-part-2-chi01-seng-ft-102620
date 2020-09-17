5292fe013c3ae5cc600939141a3ae685c62ea3fd
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-17 20:41:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,2 +1,12 @@
 def find_min_in_nested_arrays(src)
-  
+  index = 0
+  min_num = 0
+  while index < src.count do
+    arr = []
+    arr2 = []
+    min_num = (src[index]).min {|a , b| a <=> b}
+    index += 1
+  arr = min_num
+  puts arr
+  end
+end
